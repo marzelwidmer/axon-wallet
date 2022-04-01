bdc9b4c9bd2d02bd7eafac38a6fedfe100f339ad
Marcel Widmer <marzelwidmer@gmail.com>
2022-04-01 18:24:53 +0200

    chore: Add Query as HATEOAS API

---
+++ src/main/kotlin/ch/keepcalm/axon/wallet/hateoas/IndexResource.kt
@@ -0,0 +1,34 @@
+package ch.keepcalm.axon.wallet.hateoas
+
+import ch.keepcalm.axon.wallet.querymodel.WalletQueryController
+import kotlinx.coroutines.reactive.awaitSingle
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.MediaTypes
+import org.springframework.hateoas.config.EnableHypermediaSupport
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn
+import org.springframework.hateoas.support.WebStack
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping
+@EnableHypermediaSupport(stacks = [WebStack.WEBFLUX], type = [EnableHypermediaSupport.HypermediaType.HAL])
+class IndexResource() {
+
+    companion object REL {
+        const val REL_QUERY = "query"
+        const val REL_COMMAND = "command"
+    }
+
+
+    @GetMapping("/", produces = [MediaTypes.HAL_JSON_VALUE])
+    suspend fun index(): EntityModel<Unit> {
+        return EntityModel.of(Unit, linkTo(methodOn(IndexResource::class.java).index()).withSelfRel().toMono().awaitSingle())
+            .add(linkTo(methodOn(WalletQueryController::class.java).getAllWallets()).withRel(REL_QUERY).toMono().awaitSingle())
+            .add(linkTo(methodOn(WalletQueryController::class.java).getAllWallets()).withRel(REL_COMMAND).toMono().awaitSingle())
+    }
+
+}
+
--- src/main/kotlin/ch/keepcalm/axon/wallet/querymodel/WalletQueryController.kt
+++ src/main/kotlin/ch/keepcalm/axon/wallet/querymodel/WalletQueryController.kt
@@ -1,20 +1,30 @@
 package ch.keepcalm.axon.wallet.querymodel
 
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.axonframework.queryhandling.QueryGateway
-import org.springframework.web.bind.annotation.GetMapping
-import java.util.concurrent.CompletableFuture
-import ch.keepcalm.axon.wallet.querymodel.WalletView
 import ch.keepcalm.axon.wallet.coreapi.FindAllWalletsQuery
+import ch.keepcalm.axon.wallet.hateoas.IndexResource
+import kotlinx.coroutines.reactive.awaitSingle
 import org.axonframework.messaging.responsetypes.ResponseTypes
+import org.axonframework.queryhandling.QueryGateway
+import org.springframework.hateoas.CollectionModel
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.IanaLinkRelations
+import org.springframework.hateoas.MediaTypes
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.CompletableFuture
 
 @RestController
 @RequestMapping("/query")
 class WalletQueryController(private val queryGateway: QueryGateway) {
 
-    @GetMapping("/wallets")
-    fun wallets(): CompletableFuture<List<WalletView>> {
-        return queryGateway.query(FindAllWalletsQuery(), ResponseTypes.multipleInstancesOf(WalletView::class.java))
+    @GetMapping("/wallets", produces = [MediaTypes.HAL_JSON_VALUE])
+    suspend fun getAllWallets(): CollectionModel<EntityModel<WalletView>> {
+        val result : CompletableFuture<MutableList<WalletView>> = queryGateway.query(FindAllWalletsQuery(), ResponseTypes.multipleInstancesOf(WalletView::class.java))
+        return CollectionModel.wrap(result.get().asIterable())
+            .add(linkTo(methodOn(IndexResource::class.java).index()).withRel(IanaLinkRelations.PREV).toMono().awaitSingle())
     }
 }
+
