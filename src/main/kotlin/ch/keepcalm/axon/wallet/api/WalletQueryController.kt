9496505965cdfab3bbc7dd02e33602b8a8e8508e
Marcel Widmer <marzelwidmer@gmail.com>
2022-04-02 18:15:57 +0200

    chore: HATEOAS add self link on query API.

--- src/main/kotlin/ch/keepcalm/axon/wallet/api/WalletQueryController.kt
+++ src/main/kotlin/ch/keepcalm/axon/wallet/api/WalletQueryController.kt
@@ -14,7 +14,6 @@ import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.CompletableFuture
 
 @RestController
 @RequestMapping("/query")
@@ -22,9 +21,10 @@ class WalletQueryController(private val queryGateway: QueryGateway) {
 
     @GetMapping("/wallets", produces = [MediaTypes.HAL_JSON_VALUE])
     suspend fun getAllWallets(): CollectionModel<EntityModel<WalletView>> {
-        val result : CompletableFuture<MutableList<WalletView>> = queryGateway.query(FindAllWalletsQuery(), ResponseTypes.multipleInstancesOf(WalletView::class.java))
-        return CollectionModel.wrap(result.get().asIterable())
+        val result = queryGateway.query(FindAllWalletsQuery(), ResponseTypes.multipleInstancesOf(WalletView::class.java)).get()
+        return CollectionModel.wrap(result.asIterable())
             .add(linkTo(methodOn(IndexResource::class.java).index()).withRel(IanaLinkRelations.PREV).toMono().awaitSingle())
+            .add(linkTo(methodOn(WalletQueryController::class.java).getAllWallets()).withSelfRel().toMono().awaitSingle())
     }
 }
 
